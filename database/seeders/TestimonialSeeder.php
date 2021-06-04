e5249539f2a23cc85a6185a418c36d3fb89cd2fb
natchez <natchezzz@hotmail.com>
2021-06-04 14:56:28 +0200

    fin testimonials 50%

--- database/seeders/TestimonialSeeder.php
+++ database/seeders/TestimonialSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TestimonialSeeder extends Seeder
 {
@@ -13,6 +14,43 @@ class TestimonialSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('testimonials')->insert([
+            // 1
+            ['nom' => 'burg',
+            'prenom' => 'Ham',
+            'poste' => 'CEO',
+            'photo' => '01',
+            'avis' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla. Nulla sit amet luctus dolor.',
+            'created_at'=>now()],
+            // 2
+            ['nom' => 'burg',
+            'prenom' => 'Ham',
+            'poste' => 'CEO',
+            'photo' => '01',
+            'avis' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla. Nulla sit amet luctus dolor.',
+            'created_at'=>now()],
+            // 1
+            ['nom' => 'burg',
+            'prenom' => 'Ham',
+            'poste' => 'CEO',
+            'photo' => '01',
+            'avis' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla. Nulla sit amet luctus dolor.',
+            'created_at'=>now()],
+            // 1
+            ['nom' => 'burg',
+            'prenom' => 'Ham',
+            'poste' => 'CEO',
+            'photo' => '01',
+            'avis' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla. Nulla sit amet luctus dolor.',
+            'created_at'=>now()],
+            // 1
+            ['nom' => 'burg',
+            'prenom' => 'Ham',
+            'poste' => 'CEO',
+            'photo' => '01',
+            'avis' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla. Nulla sit amet luctus dolor.',
+            'created_at'=>now()],
+
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,7 @@
 @section('content')
 @include('front-end.components.carousel')
 @include('front-end.components.section1')
+@include('front-end.components.testimonial')
 @include('front-end.components.section2')
+@include('front-end.components.section3')
 @endsection
